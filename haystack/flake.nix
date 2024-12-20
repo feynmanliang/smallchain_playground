dd78e0a9f1718bd548877d27936e4b08511fbb70
Feynman Liang <feynman@blueteam.ai>
2024-12-20 09:57:22 -0800

    Adds flake for haystack

---
+++ haystack/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Python 3.12 development environment with uv";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            python312
+            python312Packages.pip
+            uv
+          ];
+
+          shellHook = ''
+            echo "Python 3.12 development environment loaded"
+            echo "uv package manager is available"
+            python --version
+          '';
+        };
+      }
+    );
+}
